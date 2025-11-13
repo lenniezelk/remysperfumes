18543a21d1343cf7be6b46654da0b2a1a0b3aa60
Kibebe <cik.cir@gmail.com>
2025-11-13 16:55:45 +0300

    updated-admin-layout-css (#5)

--- src/routes/_authed.admin.products.tsx
+++ src/routes/_authed.admin.products.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, useLocation } from '@tanstack/react-router'
 import Heading from '@/components/Heading'
 import AdminLayout from '@/components/dashboard/AdminLayout'
+import ContainerNoOverflow from '@/components/ContainerNoOverflow'
 
 export const Route = createFileRoute('/_authed/admin/products')({
   component: ProductsPage,
@@ -10,17 +11,19 @@ function ProductsPage() {
   const location = useLocation()
   return (
     <AdminLayout currentPath={location.pathname}>
-      <div>
-        <div className="flex justify-between">
-          <Heading level={4} className="text-2xl font-bold mb-4 text-brand">
-            Products
-          </Heading>
-          <button className="bg-brand text-white px-6 py-3 rounded-full text-lg font-medium mb-8 shadow-lg">
-            Add New
-          </button>
+      <ContainerNoOverflow>
+        <div className="w-[-webkit-fill-available] h-[-webkit-fill-available] p-6">
+          <div className="flex justify-between">
+            <Heading level={4} className="text-2xl font-bold mb-4 text-brand">
+              Products
+            </Heading>
+            <button className="bg-brand text-white px-6 py-3 rounded-full text-lg font-medium mb-8 shadow-lg">
+              Add New
+            </button>
+          </div>
+          <p className="text-gray-600">List view will go here...</p>
         </div>
-        <p className="text-gray-600">List view will go here...</p>
-      </div>
+      </ContainerNoOverflow>
     </AdminLayout>
   )
 }
